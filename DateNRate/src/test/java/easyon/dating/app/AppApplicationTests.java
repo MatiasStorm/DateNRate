951e49fb4f95952f0d1411ea47c68296ec84ea10
MatiasStorm <mat-435@hotmail.com>
2020-11-02 15:59:05 +0100

    Initial commit

---
+++ DateNRate/src/test/java/easyon/dating/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package easyon.dating.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
