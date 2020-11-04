0a787687e94fee6baa82eb6d8756b13c294ad9b4
Joergen Koeldal <joer1894@stud.kea.dk>
2020-11-04 16:28:49 +0100

    add database ER and normalform

--- DateNRate/src/main/java/easyon/dating/app/controllers/DemoController.java
+++ DateNRate/src/main/java/easyon/dating/app/controllers/DemoController.java
@@ -5,17 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.ResultSet;
+
 @Controller
 public class DemoController {
     private final JdbcTemplate jdbcTemplate;
 
     public DemoController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY)");
+//        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY)");
     }
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name", jdbcTemplate.queryForObject("Select * from test where navn = 'torben'", (ResultSet r, int rowCount) -> {
+            if(rowCount > 0){
+                return r.getString("navn");
+            }
+            return "Null";
+        }));
         return "index";
     }
 }
