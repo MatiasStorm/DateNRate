97ddc166f3bd9e37015c5007e3768deaed77301d
Joergen Koeldal <joer1894@stud.kea.dk>
2020-11-05 23:47:19 +0100

    created UserTagService

---
+++ DateNRate/src/main/java/easyon/dating/app/services/UserTagService.java
@@ -0,0 +1,23 @@
+package easyon.dating.app.services;
+
+import easyon.dating.app.data.UserTagDAO;
+import easyon.dating.app.models.UserTag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserTagService {
+    private final UserTagDAO userTagDAO;
+
+    @Autowired
+    public UserTagService(UserTagDAO userTagDAO) { this.userTagDAO = userTagDAO;}
+
+    public List<UserTag> getListOfUserTags() { return userTagDAO.getUserTagList(); }
+
+    public UserTag getUserTag (int userId) {
+        return userTagDAO.getUserTag(userId);
+    }
+
+}
