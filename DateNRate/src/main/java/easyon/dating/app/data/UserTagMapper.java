22cd22d55d4212b6ff0dbdee7e7286eb75bb5bb8
Joergen Koeldal <joer1894@stud.kea.dk>
2020-11-05 22:15:36 +0100

    create TagMapper, create UserTagMapper

---
+++ DateNRate/src/main/java/easyon/dating/app/data/UserTagMapper.java
@@ -0,0 +1,21 @@
+package easyon.dating.app.data;
+
+import easyon.dating.app.models.UserTag;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserTagMapper implements RowMapper<UserTag> {
+
+    @Override
+    public UserTag mapRow(ResultSet resulSet, int i) throws SQLException {
+        UserTag userTag = new UserTag();
+        userTag.setTagId(resulSet.getInt("tag_id"));
+        userTag.setUserId(resulSet.getInt("tag_name"));
+
+        return userTag;
+    }
+
+}
+
--- DateNRate/src/main/java/easyon/dating/app/models/UserTag.java
+++ DateNRate/src/main/java/easyon/dating/app/models/UserTag.java
@@ -1,4 +1,22 @@
 package easyon.dating.app.models;
 
 public class UserTag {
+    private int tagId;
+    private int userId;
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
